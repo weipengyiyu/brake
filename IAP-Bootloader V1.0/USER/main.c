d7e501b19a2ff14d5c644fd558eddef18f6475cc
weipeng <3214969473@qq.com>
2020-04-01 14:46:52 +0800

    改回flash,更新程序版本 因为SRAM掉电，程序就没了，需要重新烧写程序

---
+++ IAP-Bootloader V1.0/USER/main.c
@@ -0,0 +1,127 @@
+#include "led.h"
+#include "delay.h"
+#include "key.h"
+#include "sys.h"
+#include "lcd.h"
+#include "usart.h"
+#include "stmflash.h"
+#include "iap.h"
+ 
+ 
+/************************************************
+ ALIENTEK精英STM32开发板实验42
+ IAP实验 Bootloader V1.0 代码 
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
+
+
+int main(void)
+{		
+	u8 t;
+	u8 key;
+	u16 oldcount=0;				//老的串口接收数据值
+	u16 applenth=0;				//接收到的app代码长度
+	u8 clearflag=0;   
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+	uart_init(115200);	//串口初始化为115200
+	delay_init();	   	 	//延时初始化 
+ 	LED_Init();		  			//初始化与LED连接的硬件接口
+	KEY_Init();					//初始化按键
+ 
+
+	printf("ELITE STM32F103 ^_^\r\n");	
+	printf("input: key_up-copy key1-flash key0-sram\r\n");	
+  
+	while(1)
+	{
+	 	if(USART_RX_CNT)
+		{
+			if(oldcount==USART_RX_CNT)//新周期内,没有收到任何数据,认为本次数据接收完成.
+			{
+				applenth=USART_RX_CNT;
+				oldcount=0;
+				USART_RX_CNT=0;
+				printf("用户程序接收完成!\r\n");
+				printf("代码长度:%dBytes\r\n",applenth);
+			}else oldcount=USART_RX_CNT;			
+		}
+		t++;
+		delay_ms(10);
+		if(t==30)
+		{
+			LED0=!LED0;
+			t=0;
+			if(clearflag)
+			{
+				clearflag--;
+				if(clearflag==0)
+				{
+					;
+				}//清除显示
+			}
+		}	  	 
+		key=KEY_Scan(0);
+		if(key==WKUP_PRES)
+		{
+			delay_ms(500);
+			if(applenth)
+			{
+				printf("开始更新固件...\r\n");	
+ 				if(((*(vu32*)(0X20001000+4))&0xFF000000)==0x08000000)//判断是否为0X08XXXXXX.
+				{	 
+					iap_write_appbin(FLASH_APP1_ADDR,USART_RX_BUF,applenth);//更新FLASH代码   
+					printf("固件更新完成!\r\n");	
+				}else 
+				{  
+					printf("非FLASH应用程序!\r\n");
+				}
+ 			}else 
+			{
+				printf("没有可以更新的固件!\r\n");
+			}
+			clearflag=7;//标志更新了显示,并且设置7*300ms后清除显示									 
+		} 
+		if(key==KEY1_PRES)
+		{
+			delay_ms(500);
+			printf("开始执行FLASH用户代码 ");
+			if(((*(vu32*)(FLASH_APP1_ADDR+4))&0xFF000000)==0x08000000)//判断是否为0X08XXXXXX.
+			{	 
+				iap_load_app(FLASH_APP1_ADDR);//执行FLASH APP代码
+			}else 
+			{
+				printf("非FLASH应用程序,无法执行 ");	   
+			}									 
+			clearflag=7;//标志更新了显示,并且设置7*300ms后清除显示	  
+		}
+		if(key==KEY0_PRES)
+		{
+			delay_ms(500);
+			printf("开始执行SRAM用户代码 ");
+			if(((*(vu32*)(0X20001000+4))&0xFF000000)==0x20000000)//判断是否为0X20XXXXXX.
+			{	 
+				iap_load_app(0X20001000);//SRAM地址
+			}else 
+			{
+				printf("非SRAM应用程序,无法执行!");   
+			}									 
+			clearflag=7;//标志更新了显示,并且设置7*300ms后清除显示	 
+		}				   
+		 
+	}   	   
+}
+
+
+
+
+
+
+
+
+
+
+
